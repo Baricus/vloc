8fa5472728302353d61906c0675ba8e07e25a742
Baricus <31926270+Baricus@users.noreply.github.com>
2023-01-15 12:45:28 -0600

    moved non-pure related lemmas out of pure

--- Lib/core.v
+++ Lib/core.v
@@ -65,6 +65,61 @@ Definition UsrGhost (map : @G spec_ghost) := ∃ s, (!!(s ≠ emptyshare) * (@gh
 Definition to_tpool (exps : list iexp) : @G tpool_ghost
   := Some <$> (map_seq 0 exps).
 
+(* tpool_lookup 
+    to_tpool doesn't discard elements; for lookup's purpose it's just an extra Some
+*)
+Lemma tpool_lookup tp j : to_tpool tp !! j = Some <$> tp !! j.
+Proof.
+  unfold to_tpool. rewrite lookup_fmap.
+  by rewrite lookup_map_seq_0.
+Qed.
+
+(* tpool_lookup_Some
+    to_tpool doesn't add elements so the original tp and the tpool must agree
+*)
+Lemma tpool_lookup_Some tp j e : to_tpool tp !! j = Some (Some e) → tp !! j = Some e.
+Proof. rewrite tpool_lookup fmap_Some. naive_solver. Qed.
+
+(* Borrowed from spec_ra *)
+
+(* NOTE: G is horrible.  It's the type of the ghost state, but it's opaque by default!
+   Below is one way to see the actual type that G is. *)
+(*Compute @G tpool_ghost.*)
+
+(* to_tpool_insert
+    Inserting elements into the original tp is the same as inserting the elements
+    wrapped in "Some" to the tpool
+
+    This lets us "pull out" and "push in" updates as needed across to_tpool
+*)
+Lemma to_tpool_insert tp (j:nat) (e:iexp) :
+  (j < length tp)%nat →
+  to_tpool (<[j:=e]> tp) = <[j:=Some e]> (to_tpool tp).
+Proof.
+  intros. apply: map_eq=> i. destruct (decide (i = j)) as [->|].
+  - by rewrite tpool_lookup lookup_insert list_lookup_insert.
+  - rewrite tpool_lookup lookup_insert_ne // list_lookup_insert_ne //.
+    by rewrite tpool_lookup.
+Qed.
+(* various other versions of insertion with tpool *)
+Lemma to_tpool_insert' tp (j:nat) (e:iexp) :
+  is_Some (to_tpool tp !! j) →
+  to_tpool (<[j:=e]> tp) = <[j:=Some e]> (to_tpool tp).
+Proof.
+  rewrite tpool_lookup fmap_is_Some lookup_lt_is_Some. apply to_tpool_insert.
+Qed.
+Lemma to_tpool_snoc tp e :
+  to_tpool (tp ++ [e]) = <[length tp:=Some e]>(to_tpool tp).
+Proof.
+  intros. apply: map_eq=> i.
+  destruct (lt_eq_lt_dec i (length tp)) as [[?| ->]|?].
+  - rewrite lookup_insert_ne; last lia. by rewrite !tpool_lookup lookup_app_l.
+  - by rewrite lookup_insert tpool_lookup lookup_app_r // Nat.sub_diag.
+  - rewrite lookup_insert_ne; last lia.
+    rewrite !tpool_lookup ?lookup_ge_None_2 ?app_length //=;
+       change (ofe_car exprO) with expr; lia.
+Qed.
+
 (* to_heap
     Converts the standard heap_lang heap to the heap_ghost type.  
     This is largely just a reformatting to fit the form
@@ -72,11 +127,25 @@ Definition to_tpool (exps : list iexp) : @G tpool_ghost
 Definition to_heap (heap : gmap loc (option ival)) : @G heap_ghost :=
   fmap (λ v, (Some (fullshare, v))) heap.
 
+(* to_heap_insert
+    Inserting elements into the original heap is the same as inserting
+    the "modified" element after to_heap.
+
+    like to_tpool_insert, this lets us pull insertion across to_heap
+*)
+Lemma to_heap_insert σ l v:
+  to_heap (heap (state_upd_heap <[l:=Some v]> σ)) = <[l := Some (fullshare, Some v)]> (to_heap (heap σ)).
+Proof.
+  unfold to_heap.
+  rewrite fmap_insert.
+  auto.
+Qed.
+
 (* assert that we have a value in the heap *)
 (* heapS_mapsto
     An assertion that there is a value v in the heap at location l 
 *)
-Definition heapS_mapsto (l : loc) (v: ival) (s : share) :=
+Definition heapS_mapsto (s : share) (l : loc) (v: ival) :=
   UsrGhost (to_tpool [], {[ l := Some (s, (Some v)) ]} ).
 
 Definition spec_inv (c : cfg (heap_lang)) : mpred := 
@@ -109,4 +178,19 @@ Definition add_to_ctx id (ctx : list ectx_item) : ref_id :=
 Definition refines_right (r : ref_id) (e : iexp) := 
   (spec_ctx * (tp_id r) |=> (fill (tp_ctx r) e))%logic.
 
+(* refines_right_add_ctx
+    Filling is the same as adding something to the "forall K context" 
+*)
+Lemma refines_right_add_ctx ref K e :
+  (refines_right ref (fill K e) = refines_right (add_to_ctx ref K) e).
+Proof.
+  unfold add_to_ctx, refines_right; simpl.
+  rewrite fill_app.
+  auto.
+Qed.
+
+
 End refinement.
+
+(* hints have to go here since they don't export otherwise *)
+#[export] Hint Resolve tpool_lookup_Some : core.
