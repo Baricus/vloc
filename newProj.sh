23b6a6e5eb1b96c8c2538ad7a7ba59a1cba391e3
Baricus <31926270+Baricus@users.noreply.github.com>
2023-01-23 17:57:33 -0600

    fixed build in newProj.sh

--- newProj.sh
+++ newProj.sh
@@ -18,9 +18,12 @@ echo -n "" > $PROJ
 # print out all .v files in the folder
 for f in $FOLDERS
 do
-	(echo $f/* | tr ' ' '\n' | grep "\.v$") >> $PROJ
+	(echo $f/* | tr ' ' '\n' | grep '\.[cv]$' | sed 's/\.c$/.v/' | uniq) >> $PROJ
 	echo "" >> $PROJ
 done
 
 # add our flags
 echo "$FLAGS" >> $PROJ
+
+# for simplicity, we generate the default makefile here so we can avoid having it in the repo
+coq_makefile -f $PROJ -o Makefile
