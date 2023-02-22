61f6373273089979ec35c5fcf54fb16495d41464
Baricus <milesshamo@gmail.com>
2023-02-22 15:48:11 -0600

    thaw to thaw'' in viewshift_SEP'

--- Lib/tactics.v
+++ Lib/tactics.v
@@ -133,7 +133,7 @@ From Vloc Require Import core pure.
   
 (*NOTE: trying out making a nicer viewshift_SEP *)
 #[export] Tactic Notation "viewshift_SEP'" uconstr(a) constr(L') :=
-  let i := fresh "i" in freeze i := a; thaw i; viewshift_SEP 0 L'.
+  let i := fresh "i" in freeze i := a; thaw'' i; viewshift_SEP 0 L'.
 
 #[export] Tactic Notation "viewshift_SEP'" uconstr(a) uconstr(b) constr(L') :=
-  let i := fresh "i" in freeze i := a b; thaw i; viewshift_SEP 0 L'.
+  let i := fresh "i" in freeze i := a b; thaw'' i; viewshift_SEP 0 L'.
