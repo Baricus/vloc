4eefaa6678f4b8cd0c7292012a06fe864cfe654e
Baricus <31926270+Baricus@users.noreply.github.com>
2023-03-04 18:28:03 -0600

    Cleanup of the rev proofs

--- Proofs/rev.v
+++ Proofs/rev.v
@@ -1,31 +1,3 @@
-(* workaround for not having notation *)
-(*From iris.heap_lang Require proofmode notation.*)
-(*Require compcert.lib.Integers.*)
-(*Module iris.*)
-
-(*[> used for our modulus <]*)
-(*Import compcert.lib.Integers.*)
-(*Import proofmode notation.*)
-
-(*Definition rev_internal : val := *)
-  (*[> NOTE: there is a difference between λ: and rec:  besides notation! Recursion! <]*)
-  (*rec: "rev_internal" "prev" "cur" :=*)
-    (*match: "cur" with*)
-      (*[> return the new head of the list, the end <]*)
-      (*NONE        => "prev"*)
-      (*[> turn rest to previous and return <]*)
-    (*| SOME "node" => *)
-        (*let: "elem" := Fst !"node" in*)
-        (*let: "rest" := Snd !"node" in*)
-        (*"node" <- ("elem", "prev");;*)
-        (*"rev_internal" (SOME "node") "rest" *)
-    (*end.*)
-
-(*[> NOTE: "rev_internal" (quotes) does not work, but rev_internal does <]*)
-(*Definition iRev : val := λ: "list", (rev_internal NONE "list").*)
-
-(*End iris.*)
-
 Require Import Vloc.Lib.theory.
 Require Import Vloc.CCode.rev.
 
@@ -38,11 +10,12 @@ Definition rev_internal : heap_lang.val :=
       NONE        => "prev"
       (* turn rest to previous and return *)
     | SOME "node" => 
+        (* Due to notation differences, we need to have a few paranthesis lying about *)
         let: "elem" := Fst (!"node") in
         let: "rest" := Snd (!"node") in
         "node" <- ("elem", "prev");;
         "rev_internal" (SOME "node") "rest" 
-    end%Ei).
+    end%Ei). (* without this scope tag, this does not work; I do not know why *)
 
 (* NOTE: "rev_internal" (quotes) does not work, but rev_internal does *)
 Definition iRev : heap_lang.val := λ: "list", (rev_internal NONE "list").
@@ -51,6 +24,7 @@ Definition iRev : heap_lang.val := λ: "list", (rev_internal NONE "list").
 
 #[local] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs.  mk_varspecs prog. Defined.
+
 
 (*
    We'll need a way to compare VST lists and iris lists.
@@ -75,15 +49,15 @@ Fixpoint Vlist (sigma: list Z) (p: val) : mpred :=
 
 (* now an iris list *)
 (* NOTE: should these be notations? *)
-Definition iLit x := (LitV x).
-Definition iUnit  := (LitUnit).
-Definition iInt x := iLit (LitInt x).
-Definition iPair l r := (PairV l r).
+(*Definition iLit x := (LitV x).*)
+(*Definition iUnit  := (LitUnit).*)
+(*Definition iInt x := iLit (LitInt x).*)
+(*Definition iPair l r := (PairV l r).*)
 
 (* NOTE: we don't want an iris heap here directly, we want our simulated one. *)
 Fixpoint Ilist (sigma : list Z) v : mpred :=
   match sigma with
-  | x :: xs => ∃ (p : loc), ⌜ v = InjRV (iLit (LitLoc p)) ⌝ ∗ ∃ (v' : ival), p |-> (iPair (iInt x) v') ∗ Ilist xs v'
+  | x :: xs => ∃ (p : loc), ⌜ v = InjRV (#p) ⌝ ∗ ∃ (v' : ival), p |-> ((#x), v')%V ∗ Ilist xs v'
   | [] => ⌜ (v = InjLV (LitV LitUnit)) ⌝
   end.
 
@@ -91,22 +65,25 @@ Fixpoint Ilist (sigma : list Z) v : mpred :=
 Definition EquivList σ V I : mpred
   := (Ilist σ I * Vlist σ V)%logic.
 
-
 (* so I can stop seeing so many App constructors *)
-Notation "a <AP> b" := (App a b) (at level 21, left associativity, only parsing).
+(*Notation "a <AP> b" := (App a b) (at level 21, left associativity, only parsing).*)
 
 (*Notation "'RR' a b" := (refines_right a b) (at level 20, only printing).*)
 
 (*NOTE: gather_SEP really runs let i := fresh "i" in freeze i := L; thaw i. for any input L *)
 
+(* We'll need a bunch of helpers describing how EquivList works *)
+(* It would also be good to have stuff for Vlist and Ilist but we can wrap them up for this proof *)
 
+(* If either value is it's NULL equivalent, the list is empty *)
 Lemma Equiv_null_l σ Ival :
   EquivList σ nullval Ival |-- EquivList [] nullval Ival.
 Proof.
   destruct σ; auto.
-  (*NOTE: how to do in one step? *) iIntros "[Ri Rv]".
-  iDestruct "Rv" as (y) "[[Rnull Rtok] Rrest]".
-  fold Vlist.
+  (*NOTE: how to do in one step? *) 
+  iIntros "[Ri Rv]".
+  iDestruct "Rv" as (y) "[[Rnull Rtok] Rrest]"; fold Vlist.
+  (* I feel like this should be automatic at this point, but I don't know for sure *)
   iDestruct (field_at_ptr_neq_null with "Rnull") as "%Hcontra".
   exfalso.
   apply Hcontra.
@@ -114,7 +91,7 @@ Proof.
 Qed.
 
 Lemma Equiv_null_r σ Vval :
-  EquivList σ Vval (InjLV (iLit iUnit)) |-- (EquivList [] Vval (InjLV (iLit iUnit))).
+  EquivList σ Vval (InjLV (#())) |-- (EquivList [] Vval (InjLV (#()))).
 Proof.
   destruct σ; auto.
   iIntros "[Ril Rvl]".
@@ -122,17 +99,20 @@ Proof.
   discriminate.
 Qed.
 
+(* If the list is empty, both values are known to be null *)
 Lemma Equiv_empty Vval Ival :
-  EquivList [] Vval Ival |-- (!! (Vval = nullval) && !! (Ival = (InjLV (iLit (iUnit)))) && EquivList [] Vval Ival).
+  EquivList [] Vval Ival |-- (!! (Vval = nullval) && !! (Ival = (InjLV (#()))) && EquivList [] Vval Ival).
 Proof.
   iIntros "[%Ri %Rv]".
   auto.
 Qed.
 
 (* for the non-empty cases *)
+
+(* If either value is not null, then both aren't null and the list has a head *)
 Lemma Equiv_not_null σ Vval Ival :
-  (Vval ≠ nullval \/ Ival ≠ (InjLV (iLit iUnit))) ->
-  EquivList σ Vval Ival |-- EX (s : Z), EX (σ' : list Z), (EquivList (s :: σ') Vval Ival).
+  (Vval ≠ nullval \/ Ival ≠ (InjLV (#()))) ->
+  (EquivList σ Vval Ival |-- EX (s : Z), EX (σ' : list Z), (!! (Vval ≠ nullval /\ Ival ≠ (InjLV (#())))) &&  (EquivList (s :: σ') Vval Ival))%logic.
 Proof.
   (* exhaustive check of all the cases *)
   intros H; destruct H as [HV | HI]; destruct σ as [| s σ'].
@@ -145,16 +125,39 @@ Proof.
     contradiction.
   }
   1,2:
-    iIntros "R";
+    iIntros "[Ri Rv]";
+    iDestruct "Rv" as (Vval') "[[Rdat Rmal] Rvr]";
+    iDestruct "Ri" as (l HIval') "[%iv [Ripts Rir]]";
+    iDestruct (field_at_ptr_neq_null with "Rdat") as "%Hvpt";
     iExists s;
     iExists σ';
-    iFrame.
+    fold Vlist Ilist.
+  - iSplitR; auto.
+    + iPureIntro; split; auto.
+      rewrite HIval'.
+      auto.
+    + unfold EquivList; simpl.
+      iSplitL "Ripts Rir".
+      { iExists l. iSplitR; auto. iExists iv. iFrame.  }
+      { iExists Vval'. iFrame. }
+  - iSplitR; auto.
+    + iPureIntro; split; auto.
+      destruct (eq_dec Vval nullval); auto.
+      subst Vval.
+      exfalso.
+      apply Hvpt.
+      apply ptr_eq_nullval.
+    + unfold EquivList; simpl.
+      iSplitL "Ripts Rir".
+      { iExists l. iSplitR; auto. iExists iv. iFrame.  }
+      { iExists Vval'. iFrame. }
 Qed.
 
+(* If the list has a head, then we can pull out the head of both sublists *)
 Lemma Equiv_pop s σ' Vval Ival:
   EquivList (s :: σ') Vval Ival |-- 
-      (EX Vval', EX Ival', EX p, (
-      (!! (Ival = InjRV (iLit (LitLoc p)) /\ Vval ≠ nullval) && (data_at Ews node_t (Vint (Int.repr s),Vval') Vval * malloc_token Ews node_t Vval) * p |-> (iPair (iInt s) Ival') *
+      (EX Vval', EX Ival', EX (p : loc), (
+      (!! (Ival = InjRV (#p) /\ Vval ≠ nullval) && (data_at Ews node_t (Vint (Int.repr s),Vval') Vval * malloc_token Ews node_t Vval) * p |-> (#s, Ival')%V *
       EquivList σ' Vval' Ival'))).
 Proof.
   iIntros  "[RI RV]".
@@ -181,8 +184,10 @@ Proof.
   auto.
 Qed.
 
-Lemma EquivList_push v v' i i' l s σ:
-  ((i = InjRV (iLit (LitLoc l))) /\ (v ≠ nullval)) -> (EquivList σ v' i' * data_at Ews node_t (Vint (Int.repr s), v') v * malloc_token Ews node_t v * l |-> (iPair (iInt s) i')) |-- EquivList (s :: σ) v i.
+
+(* Likewise, if we have all the stuff to add a head, we can *)
+Lemma EquivList_push v v' i i' (l : loc) s σ:
+  ((i = InjRV (#l)) /\ (v ≠ nullval)) -> (EquivList σ v' i' * data_at Ews node_t (Vint (Int.repr s), v') v * malloc_token Ews node_t v * l |-> (#s, i')%V) |-- EquivList (s :: σ) v i.
 Proof.
   iIntros (Hnnull) "[[[[Ri Rv] Rdata] Rmalloc] Rpts]".
   destruct Hnnull as [Hi Hv].
@@ -196,27 +201,28 @@ Proof.
     iFrame.
 Qed.
   
-Lemma EquivList_local_facts:
-  forall sigma v i,
-   EquivList sigma v i |--
-       !! (is_pointer_or_null v /\ (v=nullval <-> sigma=nil) /\ (i=(InjLV (#())) <-> sigma=nil)).
+(* VST helper so we don't have to reprove things about nullval al the time *)
+Lemma EquivList_local_facts σ v i:
+   EquivList σ v i |--
+       !! (is_pointer_or_null v /\ (v=nullval <-> σ=nil) /\ (i=(InjLV (#())) <-> σ=nil)).
 Proof.
-  iIntros (σ v i) "EqList".
+  iIntros "EqList".
   destruct σ.
   - iDestruct "EqList" as "[%Ilist %Vlist]".
     subst v; subst i.
     iPureIntro.
-    split; first apply mapsto_memory_block.is_pointer_or_null_nullval.
+    split; first apply mapsto_memory_block.is_pointer_or_null_nullval. (* I hope I never apply this lemma again *)
     split; split; auto.
   - iDestruct (Equiv_pop with "EqList") as (v' i' p) "[[[[%Hi %Hv] [Hdata Hmalloc]] ptsTo] EqList']".
     subst i.
     iDestruct (data_at_valid_ptr with "Hdata") as "Hvalid"; auto.
-    { rewrite sizeof_Tstruct. simpl; lia. } (* NOTE: This is a weird goal.  Why does it come up? *)
+    { rewrite sizeof_Tstruct; simpl. lia. } (* NOTE: This is a weird goal.  Why does it come up? *)
     iSplitL.
     { destruct v; auto. }
     iPureIntro; split; split; intro; try contradiction; try list_solve.
 Qed.
 #[export] Hint Resolve EquivList_local_facts : saturate_local.
+
 
 (* The main program we want to verify *)
 Definition rev_list_internal_spec :=
@@ -227,14 +233,14 @@ Definition rev_list_internal_spec :=
       PARAMS(Vprev; Vcur)
       GLOBALS()
       SEP(EquivList Lprev Vprev Iprev ; EquivList Lcur Vcur Icur ;
-            refines_right ctx ((of_val rev_internal) <AP> (Val Iprev) <AP> (Val Icur)))
+            refines_right ctx ((of_val rev_internal) Iprev Icur))
     POST [ tptr node_t ]
       EX Vres: val, EX Ires: ival, EX σ': list Z,
       PROP()
       RETURN(Vres)
       SEP(EquivList σ' Vres Ires ; (refines_right ctx (ectxi_language.of_val Ires))).
 
-(* the equivalent wrappers *)
+(* the wrapper to just reverse a list *)
 Definition rev_list_spec :=
   DECLARE _rev_list
     WITH gv: globals, ctx: ref_id, Vhead: val, Ihead: ival, σ: list Z
@@ -243,7 +249,7 @@ Definition rev_list_spec :=
       PARAMS(Vhead)
       GLOBALS()
       SEP(EquivList σ Vhead Ihead ;
-            refines_right ctx ((of_val iRev) <AP> (Val Ihead)))
+            refines_right ctx ((of_val iRev) Ihead))
     POST [ tptr node_t ]
       EX Vres: val, EX Ires: ival, EX σ': list Z,
       PROP()
@@ -260,22 +266,19 @@ Lemma rev_internal_lemma : semax_body Vprog Gprog f_rev_list_internal rev_list_i
 Proof.
   start_function.
   unfold rev_internal.
-  (* NOTE: this works! *)
-  (*viewshift_SEP' (refines_right _ _) (EquivList Vprev Iprev).*)
+  (* NOTE: To get rid of the %Ei tag *)
+  (*Open Scope iris_expr_scope.*)
   SPR_beta.
-  fold rev_internal. (* do remember to fold this! *)
+  fold rev_internal. (* do remember to fold this! Things are messy otherwise *)
   SPR_recc; SPR_beta.
   (* either cur is null or not *)
   destruct (eq_dec Vcur nullval); subst.
   {
     (* if it is null, then the iris one is too *)
     sep_apply Equiv_null_l; sep_apply Equiv_empty.
-    (* NOTE: why do I need this here? It can't pull the emp's apart right otherwise? *)
-    autorewrite with norm.
     Intros.
     rename H into HIcurNull.
-    rewrite HIcurNull. (* cannot subst for some reason *)
-    unfold iLit, iUnit.
+    subst Icur.
 
     (* Step iris to ending *)
     SPR_inl.
@@ -305,15 +308,12 @@ Proof.
   {
     (* If it's not null, then... *)
 
-    (* NOTE: To get rid of the %Ei tag *)
-    (*Open Scope iris_expr_scope.*)
-
     (* we have an element in BOTH lists *)
     sep_apply (Equiv_not_null Lcur); [left; auto|].
     Intros c Lcur'; clear Lcur.
     sep_apply (Equiv_pop c Lcur').
     Intros Vcur' Icur' IlocCur.
-    rename H into HIlocCur; rewrite ! HIlocCur.
+    rename H0 into HIlocCur; subst Icur.
 
     (* and we can reduce the right hand side! *)
     SPR_inr.
@@ -330,8 +330,7 @@ Proof.
     SPR_beta.
 
     SPR_pairc. (* I hate this :( (also I needed the underscores again) *)
-    unfold iPair.
-    SPR_store IlocCur (#c, Iprev)%V. (* NOTE: how to get rid of the %V here? *)
+    SPR_store IlocCur (#c, Iprev)%V. (* NOTE: how to get rid of the %V here? -> needed for notation to fire *)
 
     (* now we step *)
     SPR_recc; SPR_beta.
@@ -345,11 +344,9 @@ Proof.
     forward.
     forward.
 
-
-
-    (* rebuild our list *)
+    (* we added a new element to the front of Lprev *)
     viewshift_SEP' (IlocCur |-> _) (data_at _ _ _ _) (malloc_token _ _ _) (EquivList Lprev _ _) 
-        (EquivList (c :: Lprev) Vcur Icur).
+        (EquivList (c :: Lprev) Vcur (InjRV (#IlocCur))%V).
     {
       entailer!.
       iIntros "[[[Rpt Rdta] Rmlc] Reqt]".
@@ -361,11 +358,8 @@ Proof.
     (* Almost forgot this step! *)
     SPR_injrc.
 
-    forward_call (gv, ctx, Vcur, Vcur', Icur, Icur', Lcur', (c :: Lprev)).
-    { 
-      rewrite HIlocCur. 
-      cancel.
-    }
+    (* If Ei is open here, this will break; I think it conflicts *)
+    forward_call (gv, ctx, Vcur, Vcur', (InjRV (#IlocCur))%V, Icur', Lcur', (c :: Lprev)).
 
     Intros ret.
     destruct ret as [p lret]; destruct p as [vret iret].
@@ -377,4 +371,3 @@ Proof.
     entailer!.
   }
 Qed.
-
